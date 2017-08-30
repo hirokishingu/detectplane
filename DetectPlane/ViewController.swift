56b1175ca47be52a3c4d7683ecce0a94cc5a43a5
hirokishingu <hrk.bass.526@gmail.com>
2017-08-30 22:04:38 +0900

    first commit

--- DetectPlane/ViewController.swift
+++ DetectPlane/ViewController.swift
@@ -14,8 +14,25 @@ class ViewController: UIViewController, ARSCNViewDelegate {
 
     @IBOutlet var sceneView: ARSCNView!
     
+    private let label: UILabel = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.sceneView = ARSCNView(frame:self.view.frame)
+        
+        self.label.frame = CGRect(x: 0, y:0, width: self.sceneView.frame.size.width, height: 50)
+        self.label.center = self.sceneView.center
+        self.label.textAlignment = .center
+        self.label.textColor = UIColor.red
+        self.label.font = UIFont.preferredFont(forTextStyle: .headline)
+        self.label.alpha = 0
+        
+        self.sceneView.addSubview(self.label)
+        
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
+        
+        self.view.addSubview(self.sceneView)
         
         // Set the view's delegate
         sceneView.delegate = self
@@ -24,7 +41,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         sceneView.showsStatistics = true
         
         // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
+        let scene = SCNScene()
+        
         
         // Set the scene to the view
         sceneView.scene = scene
@@ -35,6 +53,8 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
+        
+        configuration.planeDetection = .horizontal
 
         // Run the view's session
         sceneView.session.run(configuration)
@@ -53,6 +73,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
     }
 
     // MARK: - ARSCNViewDelegate
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        DispatchQueue.main.async {
+            self.label.text = "Plane Detected"
+            
+            UIView.animate(withDuration: 3.0, animations: {
+                self.label.alpha = 1.0
+            }) { (completion: Bool) in
+                self.label.alpha = 0.0
+            }
+        }
+        
+    }
     
 /*
     // Override to create and configure nodes for anchors added to the view's session.
